6210c0843e71a7138b640c25763908dacef71ac4
TELUS\T931906 <Melvin.Mathews@telus.com>
2018-07-18 15:24:40 -0700

    completed

--- app.js
+++ app.js
@@ -1,61 +1,98 @@
-// LOGIC
+////////////////////////////////////////////////LOGIC/////////////////////////////////////////////////
 
 var score = 0;
 var playerChoice;
+var win = 0;
+var loss = 0;
+var tie = 0;
+var test = 0;
 
 var readable = {
     '0': 'Rock',
-    '1': 'Scissors',
-    '2': 'Paper'
+    '1': 'Paper',
+    '2': 'Scissors'
 };
 
-var computerChoice = Math.floor(3 * Math.random());
-console.log('computerChoice: ', computerChoice)
+var computerChoice = {
+    init: function() {
+        this.store = Math.floor(3 * Math.random());
+        this.text = readable[this.store];
+    },
+    store: '',
+    text: '',
 
-var rockPaperScissors = function(playerChoice, computerChoice) {
+} 
 
 
-    if(playerChoice == 0){
-        if(computerChoice == 0){
-            var result = 'tie';
-        }
-        if(computerChoice == 1){
-            var result = 'win';
-            score++;
-            
-        }
-        if(computerChoice == 2){
-            var result = 'lose';
-            score--
-        }
+
+var order = [0, 1, 2, 0];
+
+var rockPaperScissors = function(player, computer) {
+    if(order[player] === order[computer]){
+        tie++;
+        test++;
+        
+        
+        return 'The game is tied. Try again?';
+        
     }
-
-    if(playerChoice == 1){
-        if(computerChoice == 0){
-            var result = 'lose';
-            score--
-        }
-        if(computerChoice == 1){
-            var result = 'tie';
-        }
-        if(computerChoice == 2){
-            var result = 'win';
-            score++;
-        }
+    if(order[player] === order[computer + 1]){
+        score++;
+        win++;
+        test++;
+        
+        
+        return 'You win! Play again?';
     }
-
-    if(playerChoice == 2){
-        if(computerChoice == 0){
-            var result = 'win';
-            score++;
-        }
-        if(computerChoice == 1){
-            var result = 'lose';
-            score--
-        }
-        if(computerChoice == 2){
-            var result = 'tie';
-        }
+    else{
+        score--;
+        loss++;
+        test++;
+        
+        
+        return 'You lose! Redeem yourself?';
     }
     
 }
+
+
+////////////////////////////////////////////////USER INTERFACE/////////////////////////////////////////////////
+
+
+var paragraph = document.querySelector('p');
+
+var assignClick = function(tag,pos) {
+    //assign a click listener
+    tag.addEventListener('click', function() {
+        //set the players choice
+        playerChoice = pos
+        //computer choice
+        computerChoice.init();
+        paragraph.innerText = 'The computer chose: ' + computerChoice.text
+        //determine winner
+        paragraph.innerText += '\n' + rockPaperScissors(playerChoice, computerChoice.store);
+        //display winner and score        
+        paragraph.innerText += '\n' + 'SCORE: ' + score;
+
+        console.log('wins: ', win);
+        console.log('loss: ', loss);
+        console.log('ties: ', tie);
+        console.log('test: ', test); 
+    });
+ 
+}
+
+var images = {
+    tags: document.getElementsByTagName('img'),
+    init: function() {
+        for( var step = 0; step < this.tags.length; step++){
+            assignClick(this.tags[step], step);
+        }
+    }
+
+}
+
+  
+
+images.init();
+
